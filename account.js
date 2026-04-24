8be576781a2408682409610181a09fb9324c8bee
zai <zainab.lghoul@gmail.com>
2026-04-24 11:37:15 +0200

    Enhance account management with role selection
    Added role selection for user registration and updated login logic to store logged-in user information.

--- account.js
+++ account.js
@@ -8,7 +8,6 @@ function account(tipo) {
         return;
     }
 
-
     if (tipo === "registrazione") {
         const risultato = validaInput(username, password);
         if (!risultato.valido) {
@@ -19,21 +18,19 @@ function account(tipo) {
     } else if (tipo === "login") {
         login(username, password);
     }
-
-
-
 }
 
 function registrazione(username, password) {
+    const ruolo = document.querySelector('input[name="ruolo"]:checked').value;
     let utenti = JSON.parse(localStorage.getItem("utenti")) || [];
 
     const esisteGia = utenti.find(u => u.username === username);
 
-    if(esisteGia){
+    if (esisteGia) {
         alert("Username già in uso!");
         return;
     }
-    salvataggio({username, password});
+    salvataggio({ username, password, ruolo });
     alert("Registrazione completata!");
 }
 
@@ -41,19 +38,22 @@ function login(username, password) {
     let utenti = JSON.parse(localStorage.getItem("utenti")) || [];
 
     const utenteTrovato = utenti.find(u => u.username === username && u.password === password);
-    
+
     if (!utenteTrovato) {
         alert("Riprova, credenziali errate o utente non registrato");
-
     } else {
-        alert("Bentornato, login riuscito!");
+        localStorage.setItem("utenteLoggato", JSON.stringify(utenteTrovato));
+        if (utenteTrovato.ruolo === "organizzatore") {
+            window.location.href = "index.html"; // da decidere
+        } else {
+            window.location.href = "index.html" // da decidere
+        }
     }
 }
 
-
 function validaInput(username, password) {
-    const regexUser = /^[a-zA-Z0-9]+$/;
-    const regexPswrd = /[0-9]/;
+    const regexUser = /^(?=.*[a-zA-Z])[a-zA-Z0-9._-]+$/;
+    const regexPswrd = /^(?=.*[0-9]).+$/;
 
     if (username.length < 3) {
         return { valido: false, errore: "username troppo corto, min 3 caratteri" };
@@ -66,33 +66,27 @@ function validaInput(username, password) {
     }
     if (!regexPswrd.test(password)) {
         return { valido: false, errore: "password non valida, inserire almeno un numero" };
-
     }
 
     return { valido: true };
-
 }
 
 function salvataggio(utente) {
-    let utenti = JSON.parse(localStorage.getItem("utenti")) || []; //se null usa array vuoto
+    let utenti = JSON.parse(localStorage.getItem("utenti")) || [];
     utenti.push(utente);
     localStorage.setItem("utenti", JSON.stringify(utenti));
 }
 
 function mostraLoginPage() {
-
     const main = document.querySelector("main");
 
     main.innerHTML = `
         <div class="login-container">
             <div class="login-box">
                 <h2>Login</h2>
-
                 <input id="username" type="text" placeholder="Username">
                 <input id="password" type="password" placeholder="Password">
-
                 <button onclick="account('login')">Accedi</button>
-
                 <p>Non hai un account?
                     <span onclick="mostraRegistrazionePage()" class="link">Registrati</span>
                 </p>
@@ -102,19 +96,17 @@ function mostraLoginPage() {
 }
 
 function mostraRegistrazionePage() {
-
     const main = document.querySelector("main");
 
     main.innerHTML = `
         <div class="login-container">
             <div class="login-box">
                 <h2>Registrazione</h2>
-
                 <input id="username" type="text" placeholder="Username">
                 <input id="password" type="password" placeholder="Password">
-
+                <label><input type="radio" name="ruolo" value="utente" checked> Utente</label>
+                <label><input type="radio" name="ruolo" value="organizzatore"> Organizzatore</label>
                 <button onclick="account('registrazione')">Registrati</button>
-
                 <p>Hai già un account?
                     <span onclick="mostraLoginPage()" class="link">Login</span>
                 </p>
@@ -122,3 +114,30 @@ function mostraRegistrazionePage() {
         </div>
     `;
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const utenteLoggato = JSON.parse(localStorage.getItem("utenteLoggato"));
+    const container = document.getElementById("aggiungi");
+
+    if (container && utenteLoggato && utenteLoggato.ruolo === "organizzatore") {
+        container.innerHTML = ` 
+            <button> + </button>
+        `;
+    }
+
+    if (window.location.href.includes("login.html")) {
+        mostraLoginPage();
+
+         document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            const titolo = document.querySelector(".login-box h2").textContent;
+            if (titolo === "Login") {
+                account('login');
+            } else {
+                account('registrazione');
+            }
+        }
+    });
+    }
+});
